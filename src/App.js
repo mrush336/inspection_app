93e8393a3ad76d9302a636dab719697b8ab255d3
mrush336 <mrush336@gmail.com>
2020-01-24 11:59:12 -0500

    Added gitkeep to all folders.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Header from "./Header";
-import BrakesInspection from "./components/brakes/BrakesInspection";
-import LoginControl from "./components/login/LoginControl";
 import "./App.css";
+import Header from "./Header";
+import LoginControl from "./components/login/LoginControl";
+import BrakesInspection from "./components/brakes/BrakesInspection";
+import CouplingInspection from "./components/coupling/CouplingInspection";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Header />
         <LoginControl />
         <BrakesInspection />
+        <CouplingInspection />
       </div>
     );
   }
