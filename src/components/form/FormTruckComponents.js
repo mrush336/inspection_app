f09543e78dc9f709b0850e12878a4b9adf7223d7
mrush336 <mrush336@gmail.com>
2020-01-27 20:14:28 -0500

    Added some styling and form layout.

--- src/components/form/FormTruckComponents.js
+++ src/components/form/FormTruckComponents.js
@@ -32,41 +32,51 @@ export class FormTruckComponents extends Component {
             onChange={this.handleChange}
           />
         </div>
-        <div>
-          <label>Address:</label>{" "}
-          <input
-            value={this.state.address}
-            name="address"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <label>City, State and Zip:</label>
-          <input
-            value={this.state.city_state_zip}
-            name="city_state_zip"
-            onChange={this.handleChange}
-          />
-        </div>
+
         <div>
           <label>Todays Date:</label>
           <input
+            type="text"
             value={this.state.date}
             name="date"
             onChange={this.handleChange}
           />
         </div>
+
+        <div>
+          <label>Address:</label>{" "}
+          <input
+            type="text"
+            value={this.state.address}
+            name="address"
+            onChange={this.handleChange}
+          />
+        </div>
+
         <div>
           <label>Inspectors Name:</label>
           <input
+            type="text"
             value={this.state.inspectors_name}
             name="inspectors_name"
             onChange={this.handleChange}
           />
         </div>
+
+        <div>
+          <label>City, State and Zip:</label>
+          <input
+            type="text"
+            value={this.state.city_state_zip}
+            name="city_state_zip"
+            onChange={this.handleChange}
+          />
+        </div>
+
         <div>
           <label>Vehicle Identification Number:</label>
           <input
+            type="text"
             value={this.state.vin}
             name="vin"
             onChange={this.handleChange}
